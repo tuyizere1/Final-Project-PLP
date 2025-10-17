2176a5de912e55571fd28037fe5cd79df651d450
tuyizere1 <49060920+tuyizere1@users.noreply.github.com>
2025-10-17 11:35:48 +0200

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { getAllUsers, addUser } = require('../controllers/userController');
+
+router.get('/', getAllUsers);
+router.post('/', addUser);
+
+module.exports = router;
 
