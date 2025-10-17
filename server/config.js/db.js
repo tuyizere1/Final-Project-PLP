859d684327f2924a796006bcf3333cdc2c2730a2
tuyizere1 <49060920+tuyizere1@users.noreply.github.com>
2025-10-17 11:58:58 +0200

    Update db.js

--- server/config.js/db.js
+++ server/config.js/db.js
@@ -1,12 +1,15 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
-module.exports = db;
+module.exports = pool;
+
 
