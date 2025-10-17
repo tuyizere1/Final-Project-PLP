a09f17abfb8d628f4ad6f782238415344e66c83e
tuyizere1 <49060920+tuyizere1@users.noreply.github.com>
2025-10-17 12:03:06 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/users');
-const vaccinationRoutes = require('./routes/vaccination');
-const hygieneRoutes = require('./routes/hygiene');
-
 require('dotenv').config();
+const { startReminderJob } = require('./jobs/reminderJob');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.use('/api/users', userRoutes);
-app.use('/api/vaccinations', vaccinationRoutes);
-app.use('/api/hygiene', hygieneRoutes);
+// mount routers (example)
+app.use('/api/vaccinations', require('./routes/vaccinations'));
+// ... other routes
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
+  startReminderJob();
 });
+
 
