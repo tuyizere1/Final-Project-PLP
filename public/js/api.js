eb837465515262ae38b11e367967b306350bc792
tuyizere1 <49060920+tuyizere1@users.noreply.github.com>
2025-10-17 12:03:48 +0200

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,1 +1,9 @@
+export async function postJson(url, data) {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  });
+  return res.json();
+}
 
