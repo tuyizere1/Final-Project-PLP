fe6a6e4e92ea4c605fe8ef0362775ccef803d501
tuyizere1 <49060920+tuyizere1@users.noreply.github.com>
2025-10-17 12:01:34 +0200

    Update smsService.js

--- utils/smsService.js
+++ utils/smsService.js
@@ -1,15 +1,15 @@
-const twilio = require('twilio');
 require('dotenv').config();
-
+const twilio = require('twilio');
 const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
-const sendSMS = (to, message) => {
-    return client.messages.create({
-        body: message,
-        from: process.env.TWILIO_PHONE_NUMBER,
-        to: to
-    });
-};
+async function sendSms(to, message) {
+  return client.messages.create({
+    body: message,
+    from: process.env.TWILIO_PHONE_NUMBER,
+    to
+  });
+}
 
-module.exports = { sendSMS };
+module.exports = { sendSms };
+
 
