b7276be53b361908a6a6d40b6094e32aaaec5200
tuyizere1 <49060920+tuyizere1@users.noreply.github.com>
2025-10-17 11:34:16 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/users');
+const vaccinationRoutes = require('./routes/vaccination');
+const hygieneRoutes = require('./routes/hygiene');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use('/api/users', userRoutes);
+app.use('/api/vaccinations', vaccinationRoutes);
+app.use('/api/hygiene', hygieneRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
