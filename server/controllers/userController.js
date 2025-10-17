138606401ac2b095263c93533008e1555aeca4cc
tuyizere1 <49060920+tuyizere1@users.noreply.github.com>
2025-10-17 11:37:02 +0200

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,22 @@
+const db = require('../config/db');
+
+const getAllUsers = (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if(err) return res.status(500).json({error: err});
+        res.json(results);
+    });
+};
+
+const addUser = (req, res) => {
+    const { name, phone, email, address } = req.body;
+    db.query('INSERT INTO users (name, phone, email, address) VALUES (?, ?, ?, ?)',
+        [name, phone, email, address],
+        (err, result) => {
+            if(err) return res.status(500).json({error: err});
+            res.json({message: 'User added', userId: result.insertId});
+        }
+    );
+};
+
+module.exports = { getAllUsers, addUser };
 
